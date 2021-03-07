fdb0835239f3a94757a48c23ade15b432c2a3808
Sean Gillis <Sean_Gillis1@student.uml.edu>
2021-03-06 19:31:16 -0500

    Added jumbotron to homepage. Hid tutoring settings in signup menu if tutor button not selected.

--- signUp.php
+++ signUp.php
@@ -36,7 +36,7 @@
 
             <div class="container">
                 <!-- Sign in form that will take input of username and password -->
-                <form class="signUpForm text-center center ">
+                <form class="signUpForm text-center center p-3">
                     <h5 class="mt-2 mb-4">Sign Up</h5>
                     <span class="text-danger"></span>
 
@@ -54,22 +54,24 @@
                     <input type="radio" id="yes" name="Tutor" value="yes"><label class="mr-2" for="yes">Yes</label>
                     <input type="radio" id="no" name="Tutor" value="no"><label for="no">No</label> <br>
 
-                    <label class= "mr-3 col-2" >List subjects you tutor</label>
-                    <input type="text" class="mt-2 mb-2 w-50 signUpInputBox" name="subjects"><br>
+                    <div id="tutor-settings">
+                        <label class= "mr-3 col-2" >List subjects you tutor</label>
+                        <input type="text" class="mt-2 mb-2 w-50 signUpInputBox" name="subjects"><br>
 
-                    <label class="mr-3 col-2"> Zoom Link </label>
-                    <input type="url" class= "mt-2 mb-2 w-50 signUpInputBox" name="ZoomLink"><br>
+                        <label class="mr-3 col-2"> Zoom Link </label>
+                        <input type="url" class= "mt-2 mb-2 w-50 signUpInputBox" name="ZoomLink"><br>
 
-                    <label class="mr-3 col-2"> Schedule </label><br>
-                    <select class="col-2" id="day" name="day">
-                        <option value="Monday">Monday</option>
-                        <option value="Tuesday">Tuesday</option>
-                        <option value="Wednesday">Wednesday</option>
-                        <option value="Thursday">Thursday</option>
-                        <option value="Friday">Friday</option>
-                    </select>
-                    <label class="mr-1">start</label><input type="time" class= "col-2" name="startTime">
-                    <label class="mr-1">end</label><input type="time" class= "col-2" name="endTime"><br>
+                        <label class="mr-3 col-2"> Schedule </label><br>
+                        <select class="col-2" id="day" name="day">
+                            <option value="Monday">Monday</option>
+                            <option value="Tuesday">Tuesday</option>
+                            <option value="Wednesday">Wednesday</option>
+                            <option value="Thursday">Thursday</option>
+                            <option value="Friday">Friday</option>
+                        </select>
+                        <label class="mr-1">start</label><input type="time" class= "col-2" name="startTime">
+                        <label class="mr-1">end</label><input type="time" class= "col-2" name="endTime"><br>
+                    </div>
 
                     <button type="submit" class="btn-primary mt-2 mb-2"> Sign Up</button> <br> <br>
                     <span> Already have an account? <a class="text-primary" href="signUp.php">Sign In</a></span>
@@ -82,7 +84,8 @@
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <!-- Bootstrap Javascript -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-
+        <!-- Javascript -->
+        <script src="signUp.js"></script>
     </body>
 
 </html>
